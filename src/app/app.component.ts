beab3d70499f3c7f5be195838fac9599e66f4ed5
Thithran <thi.tran.fi@gmail.com>
2019-06-21 13:10:58 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  todoArray: object[];
+  todoTitle: string;
+
+  constructor() {
+
+  }
+
+  ngOnInit(){
+    this.todoTitle = '';
+    this.todoArray = [
+    ]
+  }
+
+  addTodo(value) {
+    this.todoArray.push(value);
+  }
+
+  deleteItem(todo){
+    console.log("Delete item");
+    this.todoArray = this.todoArray.filter(item => item!== todo);
+  }
+
+  todoSubmit(value:any){ 
+    console.log(value)  
+    if (value !=="") {
+      this.todoArray.push(value.todo)
+    }
+  }
+
 }
